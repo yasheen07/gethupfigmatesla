957742f63a7afd4a7b282da9a408d7ffd0b449e8
yasheen07 <mohamedyasheen0@gamail.com>
2025-08-06 14:28:03 +0530

    feat: add pre-loader

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
+const preloader = document.getElementById('preloader');
 
-const backToTopBtn = document.getElementById('back-to-top-btn');
-
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset > 300) {
-        backToTopBtn.style.display = 'block';
-    } else {
-        backToTopBtn.style.display = 'none';
-    }
-});
-
-backToTopBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+window.addEventListener('load', () => {
+    preloader.style.display = 'none';
 });
